07a662a99730192a39f97c764d111b7177d91fac
Keon Amini <keon.amini@merico.dev>
2022-09-30 13:16:26 -0500

    refactor: removed intermediate *_schema.json files and generation

--- scripts/singer-model-generator.sh
+++ scripts/singer-model-generator.sh
@@ -12,7 +12,10 @@ fi
 package="generated"
 file_name="$tap_stream".go
 output_path="$plugin_path/models/generated/$file_name"
-json_schema_path="$(dirname "$json_path")"/"$tap_stream"_schema.json
+
+tmp_dir=$(mktemp -d -t schema-XXXXX)
+
+json_schema_path="$tmp_dir"/"$tap_stream"
 
 echo $json_schema_path
 
@@ -24,11 +27,12 @@ cat "$json_path" |  jq '
 sed -i -r "/\"null\",/d" "$json_schema_path"
 sed -i -r "/.*additionalProperties.*/d" "$json_schema_path"
 
+# see output
+cat "$json_schema_path" | jq -r
+
 exitcode=$?
 if [ $exitcode != 0 ]; then
   exit $exitcode
 fi
 
-tmp_dir=$(mktemp -d -t schema-XXXXX)
-cp "$json_schema_path" "$tmp_dir"/"$tap_stream"
-gojsonschema -v -p "$package" "$tmp_dir"/"$tap_stream" -o "$output_path"
+gojsonschema -v -p "$package" "$json_schema_path" -o "$output_path"
