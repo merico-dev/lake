67d525f63feddbc5a636ef59da6ee18890989eec
Klesh Wong <zhenmian.huang@merico.dev>
2023-09-05 13:51:46 +0800

    refactor: simplify tapd scope config

--- backend/plugins/tapd/models/scope_config.go
+++ backend/plugins/tapd/models/scope_config.go
@@ -18,17 +18,15 @@ limitations under the License.
 package models
 
 import (
-	"encoding/json"
-
 	"github.com/apache/incubator-devlake/core/models/common"
 )
 
 type TapdScopeConfig struct {
 	common.ScopeConfig `mapstructure:",squash" json:",inline" gorm:"embedded"`
-	ConnectionId       uint64          `mapstructure:"connectionId" json:"connectionId"`
-	Name               string          `gorm:"type:varchar(255);index:idx_name_tapd,unique" validate:"required" mapstructure:"name" json:"name"`
-	TypeMappings       json.RawMessage `mapstructure:"typeMappings,omitempty" json:"typeMappings"`
-	StatusMappings     json.RawMessage `mapstructure:"statusMappings,omitempty" json:"statusMappings"`
+	ConnectionId       uint64            `mapstructure:"connectionId" json:"connectionId"`
+	Name               string            `gorm:"type:varchar(255);index:idx_name_tapd,unique" validate:"required" mapstructure:"name" json:"name"`
+	TypeMappings       map[string]string `mapstructure:"typeMappings,omitempty" json:"typeMappings" gorm:"serializer:encdec"`
+	StatusMappings     map[string]string `mapstructure:"statusMappings,omitempty" json:"statusMappings" gorm:"serializer:encdec"`
 }
 
 func (t TapdScopeConfig) TableName() string {
