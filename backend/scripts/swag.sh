8f0eb1b4c3213a185a3adecb086fd90e25bed020
Klesh Wong <zhenmian.huang@merico.dev>
2023-10-31 11:59:14 +0800

    chore: fixing swag ci

--- backend/scripts/swag.sh
+++ backend/scripts/swag.sh
@@ -38,7 +38,7 @@ fi
 
 swag -v
 ls -l
-go get
+go get ./...
 echo swag init --parseDependency --parseInternal -o $ROOT_DIR/server/api/docs -g $GENERAL_API_INFO_PATH -d $DOC_DIRS
 swag init --parseDependency --parseInternal -o $ROOT_DIR/server/api/docs -g $GENERAL_API_INFO_PATH -d $DOC_DIRS
 echo "visit the swagger document on http://localhost:8080/swagger/index.html";
